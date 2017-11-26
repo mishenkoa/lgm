cf3b486e30fc08c426ae4ffd53196d82b2353e1a
YourMoron <leshka.mishenko@gmail.com>
2017-11-26 14:14:53 +0500

    initial commit

---
+++ desktop/src/com/kuraleses/lgm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kuraleses.lgm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kuraleses.lgm.LgmMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LgmMain(), config);
+	}
+}
